f4d1ceda31344c485ea53ae75587c6c1085a3b0e
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudAdvisorynotificationsV1/NotificationType.swift
+++ Sources/GoogleCloudAdvisorynotificationsV1/NotificationType.swift
@@ -77,7 +77,7 @@ public enum NotificationType: Codable, Equatable, Sendable {
 
   /// Initialize from a string value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownStringValue`](doc:NotificationType/unknownStringValue(_:)).
   public init(stringValue: Swift.String) {
     switch stringValue {
     case "NOTIFICATION_TYPE_UNSPECIFIED": self = .unspecified
@@ -91,7 +91,7 @@ public enum NotificationType: Codable, Equatable, Sendable {
 
   /// Initialize from an integer value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownIntValue`](doc:NotificationType/unknownIntValue(_:)).
   public init(intValue: Int) {
     switch intValue {
     case 0: self = .unspecified
--- Sources/GoogleCloudAdvisorynotificationsV1/NotificationView.swift
+++ Sources/GoogleCloudAdvisorynotificationsV1/NotificationView.swift
@@ -71,7 +71,7 @@ public enum NotificationView: Codable, Equatable, Sendable {
 
   /// Initialize from a string value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownStringValue`](doc:NotificationView/unknownStringValue(_:)).
   public init(stringValue: Swift.String) {
     switch stringValue {
     case "NOTIFICATION_VIEW_UNSPECIFIED": self = .unspecified
@@ -83,7 +83,7 @@ public enum NotificationView: Codable, Equatable, Sendable {
 
   /// Initialize from an integer value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownIntValue`](doc:NotificationView/unknownIntValue(_:)).
   public init(intValue: Int) {
     switch intValue {
     case 0: self = .unspecified
